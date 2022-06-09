7f9bcda49bb0e7d6eefc75eb1e59051315c6832f
Nguyễn Phúc Thuần <phucthuan1st@gmail.com>
2022-06-09 11:05:46 +0700

    valid expression, rewrite expression to prevent crash

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,28 +10,31 @@ class ViewController : UIViewController
 {
     
     @IBOutlet weak var calcArea: UILabel!
-    @IBOutlet weak var calcResult: UILabel!
+    @IBOutlet weak var resultArea: UILabel!
     
     var calcContent:String = ""
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         clearAll()
+        super.viewDidLoad()
     }
     
     func clearAll() {
         calcArea.text = ""
-        calcResult.text = ""
+        resultArea.text = ""
         calcContent = ""
+        print("All clear")
     }
     
     func addContent(value: String) {
         calcContent = calcContent + value
+        print("Button hit -> Change content to \(calcContent)")
         calcArea.text = calcContent
     }
     
     func clearLast() {
         calcContent.removeLast()
+        print("Clear last character")
         calcArea.text = calcContent
     }
     
@@ -44,20 +47,18 @@ class ViewController : UIViewController
     }
     
     @IBAction func tapEquals(_sender: Any) {
-        let validExpression = validInput()
+        let (check, result) = Expression.getExpressionValue(expression: calcContent)
         
-        if (validExpression) {
-            let expression = calcContent.replacingOccurrences(of: "%", with: "*0.01")
-            let resultExpression = NSExpression(format: expression + ".00")
-            let result = resultExpression.expressionValue(with: nil, context: nil) as! Double
+        if (check == true) {
             
             var format:String = "%f"
             if result.rounded() == result {
                 format = "%.0f"
             }
+            
             let resultString:String = String(format: format, result)
             
-            calcResult.text = resultString
+            resultArea.text = resultString
             calcArea.text = ""
             calcContent = ""
         }
@@ -69,57 +70,6 @@ class ViewController : UIViewController
             alert.addAction(UIAlertAction(title: "Go back", style: .default))
             self.present(alert, animated: true)
         }
-    }
-    
-    func validInput() -> Bool {
-        var count = 0
-        var charIndex = [Int]()
-        
-        for char in calcContent {
-            if (specialCharacter(char: char)) {
-                charIndex.append(count)
-            }
-            count += 1
-        }
-        
-        var previosIndex:Int = -1
-        
-        for index in charIndex {
-            if (index == 0) {
-                return false
-            }
-            
-            if index == calcContent.count - 1 {
-                return false
-            }
-            
-            if (previosIndex != -1) {
-                if index - previosIndex == 1 {
-                    return false
-                }
-            }
-            
-            previosIndex = index
-        }
-        
-        return true
-    }
-    
-    func specialCharacter(char: Character) -> Bool {
-        if char == "+" {
-            return true
-        }
-        if char == "*" {
-            return true
-        }
-        if char == "/" {
-            return true
-        }
-        if char == "%" {
-            return true
-        }
-        
-        return false
     }
     
     @IBAction func tapOne(_sender: Any) {
@@ -188,6 +138,119 @@ class ViewController : UIViewController
         addContent(value: "/")
     }
     
-    
 }
 
+class Char {
+    public static func specialCharacter(char: Character) -> Bool {
+        if char == "+" {
+            return true
+        }
+        if char == "*" {
+            return true
+        }
+        if char == "/" {
+            return true
+        }
+        if char == "-" {
+            return true
+        }
+        
+        return false
+    }
+    
+    public static func isANumber(char: Character) -> Bool {
+        return char.isNumber
+    }
+}
+
+class Expression {
+    
+    // check if an expression is valid
+    static func validExpression(expression: String) -> Bool {
+        
+        var charArr = [Character]()
+        for char in expression {
+            charArr.append(char)
+        }
+        var i = 1
+        while i < (charArr.count - 2) {
+            if (Char.specialCharacter(char: charArr[i])) {
+                if Char.specialCharacter(char: charArr[i-1]) || (i < charArr.count && Char.specialCharacter(char: charArr[i+1])) {
+                    return false
+                }
+            }
+
+            if (charArr[i] == "%") {
+                if !Char.isANumber(char: charArr[i-1]) ||
+                    (i < charArr.count - 1 && !Char.specialCharacter(char: charArr[i+1])) {
+                    return false
+                }
+            }
+            
+            i = i + 1
+        }
+        
+        if Char.specialCharacter(char: charArr[charArr.count - 1]) || Char.specialCharacter(char: charArr[0]) {
+            return false
+        }
+        
+        return true
+    }
+    
+    // rewrite expression for exactly calculating
+    static func reWriteExpression(expression: String) -> String {
+        var charArr = [Character]()
+        
+        for char in expression {
+            charArr.append(char)
+        }
+        
+        var i = 1
+        while i < (charArr.count) {
+            print(charArr[i])
+            if (charArr[i] == "%") {
+                
+                var j = i - 1
+                
+                while j > 0 && !Char.specialCharacter(char: charArr[j]) {
+                    j = j - 1
+                }
+                
+                if Char.specialCharacter(char: charArr[j]) {
+                    charArr.insert(")", at: i + 1)
+                    charArr.insert("(", at: j + 1)
+                }
+                
+                i = i + 2
+            }
+            else {
+                i = i + 1
+            }
+        }
+        
+        
+        var ex:String = ""
+        for char in charArr {
+            ex = ex + String(char)
+        }
+        
+        ex = "1.0*(" + ex.replacingOccurrences(of: "%", with: "*0.01") + ")"
+        print("Expression after rewrite: \(ex)")
+        
+        return ex
+    }
+    
+    // calculate the expression
+    public static func getExpressionValue(expression: String) -> (Bool, Double) {
+        let check:Bool = validExpression(expression: expression)
+        var result:Double = 0
+        
+        if (check) {
+            let ex = reWriteExpression(expression: expression)
+            let resultExpression = NSExpression(format: ex)
+            result = resultExpression.expressionValue(with: nil, context: nil) as! Double
+        }
+        
+        return (check, result)
+    }
+}
--- CalculatorTests/CalculatorTests.swift
+++ CalculatorTests/CalculatorTests.swift
@@ -10,27 +10,34 @@ import XCTest
 
 class CalculatorTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSpecialCharacter() throws {
+        XCTAssertEqual(true, Char.specialCharacter(char: "+"))
+        XCTAssertEqual(true, Char.specialCharacter(char: "-"))
+        XCTAssertEqual(true, Char.specialCharacter(char: "*"))
+        XCTAssertEqual(true, Char.specialCharacter(char: "/"))
+        XCTAssertEqual(false, Char.specialCharacter(char: "%"))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testIsANumber() throws {
+        XCTAssertEqual(false, Char.isANumber(char: "n"))
+        XCTAssertEqual(true, Char.isANumber(char: "1"))
+        XCTAssertEqual(false, Char.isANumber(char: "h"))
+        XCTAssertEqual(true, Char.isANumber(char: "9"))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testValidExpression() {
+        XCTAssertEqual(true, Expression.validExpression(expression: "9+3/2*4+7"))
+        XCTAssertEqual(false, Expression.validExpression(expression: "-3"))
+        XCTAssertEqual(true, Expression.validExpression(expression: "3+8%"))
+        XCTAssertEqual(false, Expression.validExpression(expression: "432*453+++332"))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testReWriteExpression() {
+        XCTAssertEqual("9/(90*0.01)+5/(50*0.01)", Expression.reWriteExpression(expression: "9/90%+5/50%"))
+    }
+    
+    func testExpressionValue() {
+        XCTAssertEqual(20, Expression.getExpressionValue(expression: "9/90%+5/50%"))
     }
 
 }
